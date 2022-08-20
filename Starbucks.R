616332889113b39c20a00d296f1c3fcd312875bf
theophilus2021 <torokingdom@protonmail.com>
2022-08-19 22:54:08 -0500

    Rename columns, convert chr to numeric
    Rename columns, convert chr to numeric

--- Starbucks.R
+++ Starbucks.R
@@ -1,5 +1,5 @@
 # Step 0
-install.packages("randomForest", repos = "https://cran.microsoft.com/")
+#install.packages("randomForest", repos = "https://cran.microsoft.com/")
 
 # Load libraries
 library(caret)
@@ -17,14 +17,48 @@ head(SBUX_Data)
 
 # Step 1: Data cleaning 
 
+# Delete, fill NULL with averages, etc
+
+
+
 # Step 2: Data exploration and analysis
 # 2.1 structure, features, target
 dim(SBUX_Data)
 str(SBUX_Data)
 view(SBUX_Data)
 
-# 2.2 plots and correlation 
-# 2.2.a turn features to numeric 
+# Rename columns 
+names(SBUX_Data)<-c("Beverage_cat","Beverage","Beverage_prep",
+                    "Calories","Total_Fat","Trans_Fat","Sat_Fat",
+                    "Sodium","Carb","Chole","Fibre","Sugars","Protein",
+                    "VitA","VitC","Calcium","Iron","Caffeine")
+
+# Remove % then convert chr to numeric
+SBUX_Data$VitA<-gsub("%","", as.character(SBUX_Data$VitA))
+SBUX_Data$VitC<-gsub("%","", as.character(SBUX_Data$VitC))
+SBUX_Data$Calcium<-gsub("%","", as.character(SBUX_Data$Calcium))
+SBUX_Data$Caffeine<-gsub("%","", as.character(SBUX_Data$Caffeine))
+SBUX_Data$Iron<-gsub("%","", as.character(SBUX_Data$Iron))
+
+SBUX_Data$VitA <-as.numeric(SBUX_Data$VitA)
+SBUX_Data$VitC <-as.numeric(SBUX_Data$VitC)
+SBUX_Data$Calcium <-as.numeric(SBUX_Data$Calcium)
+SBUX_Data$Caffeine <-as.numeric(SBUX_Data$Caffeine)
+SBUX_Data$Iron <-as.numeric(SBUX_Data$Iron)
+SBUX_Data$Total_Fat <-as.numeric(SBUX_Data$Total_Fat)
+
+summary(SBUX_Data$VitA)
+
+histogram(SBUX_Data$VitA)
+SBUX_Data$VitC
+
+# 2.2 Eliminate features/attributes not useful 
+SBUX <- SBUX_data %>% select(c())
+
+
+# 2.4.a turn features to numeric 
+# 2.5 plots and correlation 
+
 
 
 
