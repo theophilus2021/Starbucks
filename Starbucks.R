7e4a00a1badee0563d51d71280ff243ceac1d00b
theophilus2021 <torokingdom@protonmail.com>
2022-08-19 20:41:18 -0500

    Added data exploration
    Added data exploration

--- Starbucks.R
+++ Starbucks.R
@@ -11,6 +11,26 @@ library(randomForest)
 # Set random seed
 set.seed(123)
 
+
 SBUX_Data<-read.csv("starbucks_drinkMenu_expanded.csv", stringsAsFactors = FALSE)
 head(SBUX_Data)
+
+# Step 1: Data cleaning 
+
+# Step 2: Data exploration and analysis
+# 2.1 structure, features, target
 dim(SBUX_Data)
+str(SBUX_Data)
+view(SBUX_Data)
+
+# 2.2 plots and correlation 
+# 2.2.a turn features to numeric 
+
+
+
+# correlation table 
+cor(SBUX_Data[15:18])
+
+# scatterplot 
+pairs(SBUX_Data[15:18])
+
