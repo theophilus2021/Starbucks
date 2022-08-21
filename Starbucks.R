7e21b3940603f6ed2097b4b6fdd172b4799ae837
theophilus2021 <torokingdom@protonmail.com>
2022-08-20 23:38:13 -0500

    Added regression models
    Added regression models

--- Starbucks.R
+++ Starbucks.R
@@ -19,6 +19,12 @@ head(SBUX_Data)
 
 # Step 1: Data cleaning 
 
+# Rename columns 
+names(SBUX_Data)<-c("Beverage_cat","Beverage","Beverage_prep",
+                    "Calories","Total_Fat","Trans_Fat","Sat_Fat",
+                    "Sodium","Carb","Chole","Fibre","Sugars","Protein",
+                    "VitA","VitC","Calcium","Iron","Caffeine")
+
 # Fill NA with 0, etc 
 # Set 0 caffeine to Iced Tea, Chocolate Smoothies,23 NAs 
 SBUX_Data$Caffeine[is.na(SBUX_Data$Caffeine)]<-0
@@ -32,11 +38,6 @@ dim(SBUX_Data)
 str(SBUX_Data)
 view(SBUX_Data)
 
-# Rename columns 
-names(SBUX_Data)<-c("Beverage_cat","Beverage","Beverage_prep",
-                    "Calories","Total_Fat","Trans_Fat","Sat_Fat",
-                    "Sodium","Carb","Chole","Fibre","Sugars","Protein",
-                    "VitA","VitC","Calcium","Iron","Caffeine")
 
 # Remove % then convert chr to numeric
 SBUX_Data$VitA<-gsub("%","", as.character(SBUX_Data$VitA))
@@ -63,13 +64,42 @@ boxplot(SBUX_Data$Iron)
 # 2.5 plots and correlation 
 
 # correlation table 
-cor(SBUX_Data[4:18])
+cor(SBUX_Data[4:9])
+cor(SBUX_Data[8:13])
+cor(SBUX_Data[12:18])
+
 
 # correlation plots 
-pairs(SBUX_Data[4:10])
-pairs(SBUX_Data[10:18])
+pairs(SBUX_Data[4:9])
+pairs(SBUX_Data[8:13])
+pairs(SBUX_Data[12:18])
 
 # 2.6 Eliminate features/attributes not useful 
 #SBUX <- SBUX_data %>% select(c())
+
+# Parition 70% for training
+Train<-createDataPartition(SBUX_Data$Sugars, p=.70, list = FALSE)
+Train_SBUX<-SBUX_Data[Train,] 
+
+# Parition 30% for valuation and test 
+Val_Test_SBUX<-SBUX_Data[-Train,]
+
+# Parition 15% for valuation 
+Val_SBUX<-createDataPartition(Val_Test_SBUX$Sugars, p=.50, list = FALSE)
+
+# Parition 15% for test  
+Test_SBUX<-Val_Test_SBUX[-Train,]
+
+# Model in terms of sugars
+Model_sugars<-lm(Sugars~Calories+Total_Fat+Trans_Fat+Sat_Fat+Sodium+Carb+Chole+Fibre+Protein+VitA+VitC+Calcium+Iron+Caffeine,Train_SBUX)
+summary(Model_sugars)
+
+# Model in terms of calories
+Model_calories<-lm(Calories~Sugars+Total_Fat+Trans_Fat+Sat_Fat+Sodium+Carb+Chole+Fibre+Protein+VitA+VitC+Calcium+Iron+Caffeine,Train_SBUX)
+summary(Model_calories)
+
+# Model in terms of protein
+Model_protein<-lm(Protein~Calories+Sugars+Total_Fat+Trans_Fat+Sat_Fat+Sodium+Carb+Chole+Fibre+VitA+VitC+Calcium+Iron+Caffeine,Train_SBUX)
+summary(Model_protein)
 
 
